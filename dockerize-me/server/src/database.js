4a6a06fcc08ea2539990e300575309ea580fe82a
Marco Fortunato <70582878+crox23@users.noreply.github.com>
2021-09-29 09:50:31 -0500

    1st commit for upload github for MIT check, later needed to be modify

---
+++ dockerize-me/server/src/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const User = require('./models/user.model');
+
+const connection = 'mongodb://mongo:27017/dockerize-me';
+//const connection = 'mongodb://localhost:27017/dockerize-me';
+
+const connectDb = () => {
+  return mongoose.connect(connection);
+};
+
+module.exports = connectDb;
+
